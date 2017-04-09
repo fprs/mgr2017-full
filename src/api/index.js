47fc8ae4ec709daa9bb90eb0edc782c99b9dcbce
Piotr Frasiński <fprs@users.noreply.github.com>
2017-04-09 13:26:43 +0200

    Initial commit

---
+++ src/api/index.js
@@ -0,0 +1,30 @@
+import { version } from '../../package.json';
+import { Router } from 'express';
+import facets from './facets';
+
+export default ({ config, db }) => {
+	let api = Router();
+
+	// mount the facets resource
+	api.use('/facets', facets({ config, db }));
+
+	// perhaps expose some API metadata at the root
+	api.get('/', (req, res) => {
+		console.log(version, 'version')
+		res.json({ version });
+	});
+
+	api.get('/test', (req,res) => {
+		res.json({ data: 'test' })
+	})
+
+	api.get('/redis', (req,res) => {
+		res.json({ data: 'redis' })
+	})
+
+	api.get('/mongo', (req,res) => {
+		res.json({ data: 'mongo' })
+	})
+
+	return api;
+}
