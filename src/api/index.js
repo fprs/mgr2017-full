6f50053525ec9fb6ad6088ca1850617526e8320c
Piotr Frasiński <fprs@users.noreply.github.com>
2017-05-07 13:38:06 +0200

    reading data from mongo

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,8 @@
 import { version } from '../../package.json';
 import { Router } from 'express';
 import facets from './facets';
+import { MongoClient } from 'mongodb'
+import co from 'co'
 
 export default ({ config, db }) => {
 	let api = Router();
@@ -14,16 +16,43 @@ export default ({ config, db }) => {
 		res.json({ version });
 	});
 
-	api.get('/sql', (req, res) => {
-		res.json({ data: 'sql' })
-	})
-
-	api.get('/redis', (req, res) => {
-		res.json({ data: 'redis' })
+	['sql', 'redis'].map(databaseName => {
+		api.get(`/${databaseName}`, (req, res) => {
+			setTimeout(() => res.json({ data: databaseName }), Math.random()*800+300)
+		})
+		api.post(`/${databaseName}`, (req, res) => {
+			setTimeout(() => res.json({ data: databaseName }), Math.random()*800+300)
+		})
 	})
 
 	api.get('/mongo', (req, res) => {
-		res.json({ data: 'mongo' })
+
+		co(function*() {
+			const db = yield MongoClient.connect('mongodb://127.0.0.1:27017/test')
+			console.log('Connected to the mongoDB server')
+
+			const col = db.collection('testCollection')
+
+			const cursor = col.find()
+
+			let printCursor = []
+
+			while (yield cursor.hasNext()) {
+				const doc = yield cursor.next()
+				printCursor.push(doc)
+				console.log(doc)
+			}
+			db.close()
+			res.json({ resp: 'good', cursor: printCursor })
+		}).catch(err => console.log(err.stack))
+
+
+		// const url = 'mongodb://127.0.0.1:27017/test'
+		// MongoClient.connect(url, (err, db) => {
+		// 	const collection = db.collection('testCollection').find()
+		// 	console.log('2', collection)
+		// 	res.json({ test: 'aa' })
+		// })
 	})
 
 	return api;
